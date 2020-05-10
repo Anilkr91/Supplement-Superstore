3f0090842a06271ba3ee3a7ba1aa0247034255f1
Anilkr91 <anilklal91@gmail.com>
2020-05-10 10:02:16 +0530

    ui fixes + imageZoom

---
+++ Supplement Superstores/ImageZoomVC.swift
@@ -0,0 +1,84 @@
+//
+//  ImageZoomVC.swift
+//  Supplement Superstores
+//
+//  Created by mac on 10/05/20.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import UIKit
+import Kingfisher
+
+class ImageZoomVC: UIViewController, UIScrollViewDelegate {
+    
+    var imageView: UIImageView!
+    var scrollImg: UIScrollView!
+    var imageUrl: URL?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupViews()
+        
+    }
+    
+    func setupViews() {
+        let vWidth = self.view.frame.width
+        let vHeight = self.view.frame.height
+        
+        scrollImg = UIScrollView()
+        scrollImg.delegate = self
+        scrollImg.frame = CGRect(x: 0, y: 0, width: vWidth, height: vHeight)
+        scrollImg.backgroundColor = UIColor(red: 90, green: 90, blue: 90, alpha: 0.90)
+        scrollImg.alwaysBounceVertical = false
+        scrollImg.alwaysBounceHorizontal = false
+        scrollImg.showsVerticalScrollIndicator = true
+        scrollImg.flashScrollIndicators()
+        scrollImg.minimumZoomScale = 0.2
+        scrollImg.maximumZoomScale = 2.0
+        
+        
+        
+        let doubleTapGest = UITapGestureRecognizer(target: self, action: #selector(handleDoubleTapScrollView(recognizer:)))
+        doubleTapGest.numberOfTapsRequired = 2
+        scrollImg.addGestureRecognizer(doubleTapGest)
+        
+        self.view.addSubview(scrollImg)
+        
+        imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: vWidth, height: vHeight))
+        
+        let resource = ImageResource(downloadURL: imageUrl!, cacheKey: "\(imageUrl!)")
+        imageView.kf.setImage(with: resource)
+        //imageView.image = UIImage(named: "home_banner.png")
+        imageView.contentMode = .scaleAspectFit
+        imageView!.layer.cornerRadius = 11.0
+        imageView!.clipsToBounds = false
+        scrollImg.addSubview(imageView!)
+        
+    }
+    
+    @objc func doneButtonClicked(_ button:UIBarButtonItem!){
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func handleDoubleTapScrollView(recognizer: UITapGestureRecognizer) {
+        if scrollImg.zoomScale == 1 {
+            scrollImg.zoom(to: zoomRectForScale(scale: scrollImg.maximumZoomScale, center: recognizer.location(in: recognizer.view)), animated: true)
+        } else {
+            scrollImg.setZoomScale(1, animated: true)
+        }
+    }
+    
+    func zoomRectForScale(scale: CGFloat, center: CGPoint) -> CGRect {
+        var zoomRect = CGRect.zero
+        zoomRect.size.height = imageView.frame.size.height / scale
+        zoomRect.size.width  = imageView.frame.size.width  / scale
+        let newCenter = imageView.convert(center, from: scrollImg)
+        zoomRect.origin.x = newCenter.x - (zoomRect.size.width / 2.0)
+        zoomRect.origin.y = newCenter.y - (zoomRect.size.height / 2.0)
+        return zoomRect
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return self.imageView
+    }
+}
--- Supplement Superstores/ProductDetail/ProductDetailInfoTVCell.swift
+++ Supplement Superstores/ProductDetail/ProductDetailInfoTVCell.swift
@@ -11,19 +11,16 @@ import UIKit
 import Kingfisher
 import FSPagerView
 
+protocol ProductDetailDelegate: class {
+    func didProductImageTap(imageUrl: URL)
+    
+}
+
 class ProductDetailInfoTVCell: UITableViewCell {
     var products: [ProductImageModel] = []
+    weak var delegate: ProductDetailDelegate?
     
-   @IBOutlet weak var pageControl: FSPageControl!
-//        didSet {
-//
-//            self.pageControl.numberOfPages = self.products.count
-//            self.pageControl.contentHorizontalAlignment = .center
-//            self.pageControl.contentInsets = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
-//
-//        }
-//    }
-    
+    @IBOutlet weak var pageControl: FSPageControl!
     @IBOutlet weak var pagerView: FSPagerView! {
         didSet {
             self.pagerView.register(PagerViewCell.self, forCellWithReuseIdentifier: "cell")
@@ -34,6 +31,7 @@ class ProductDetailInfoTVCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+       
        
         pagerView.itemSize = CGSize(width: self.frame.size.width, height: self.frame.size.height - 80)
         pagerView.automaticSlidingInterval = 5.0
@@ -47,8 +45,12 @@ class ProductDetailInfoTVCell: UITableViewCell {
     
     
     func updateCell(products: [ProductImageModel]) {
+       // setupCarousel(products: products)
         self.products = products
         self.pageControl.hidesForSinglePage = true
+        let color = UIColor(red: 22/255.0, green: 125/255.0, blue: 210/255.0, alpha: 1)
+        pageControl.setFillColor( color, for: .normal)
+       // self.pageControl.tintColor = , alpha: 1)
         self.pageControl.numberOfPages = products.count
         self.pageControl.contentHorizontalAlignment = .center
         self.pageControl.contentInsets = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
@@ -60,11 +62,11 @@ class PagerViewCell: FSPagerViewCell {
     override func didMoveToSuperview() {
         super.didMoveToSuperview()
 
-        // remove the default shadow.
         self.contentView.layer.shadowColor = nil
         self.contentView.layer.shadowRadius = 0
         self.contentView.layer.shadowOpacity = 0
         self.contentView.layer.shadowOffset = .zero
+    
     }
 }
 
@@ -74,12 +76,13 @@ public func numberOfItems(in pagerView: FSPagerView) -> Int {
     return self.products.count
 }
 
-public func pagerView(_ pagerView: FSPagerView, cellForItemAt index: Int) -> FSPagerViewCell {
+    public func pagerView(_ pagerView: FSPagerView, cellForItemAt index: Int) -> FSPagerViewCell {
      let cell = pagerView.dequeueReusableCell(withReuseIdentifier: "cell", at: index)
-            
+
     let Url = URL(string: products[index].image ?? "")
     let resource = ImageResource(downloadURL: Url!, cacheKey: "\(Url!)")
     cell.imageView?.kf.setImage(with: resource)
+    //cell.imageView?.image = UIImage(named: "home_banner.png")
     cell.imageView?.contentMode = .scaleAspectFit
     //cell.imageView?.clipsToBounds = true
     return cell
@@ -87,15 +90,22 @@ public func pagerView(_ pagerView: FSPagerView, cellForItemAt index: Int) -> FSP
 
 // MARK:- FSPagerView Delegate
 
-func pagerView(_ pagerView: FSPagerView, didSelectItemAt index: Int) {
-
-}
-
-func pagerViewWillEndDragging(_ pagerView: FSPagerView, targetIndex: Int) {
-       self.pageControl.currentPage = targetIndex
-   }
+//    func pagerView(_ pagerView: FSPagerView, shouldHighlightItemAt index: Int) -> Bool {
+//        return false
+//    }
+    
+    func pagerView(_ pagerView: FSPagerView, didSelectItemAt index: Int) {
+        pagerView.deselectItem(at: index, animated: true)
+        if let delegate = delegate {
+            let Url = URL(string: products[index].image ?? "")!
+            delegate.didProductImageTap(imageUrl: Url)
+            
+        }
+    }
     
     func pagerViewDidScroll(_ pagerView: FSPagerView) {
         self.pageControl.currentPage = pagerView.currentIndex
     }
 }
+
+ 
